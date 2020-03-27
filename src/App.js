4d58c099ce7aed3624eac7c0aa9188601093a131
Konstantin Kornev <sunyris@gmail.com>
2020-03-28 12:15:03 +1300

    create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Map from "./Map";
+
+function App() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
