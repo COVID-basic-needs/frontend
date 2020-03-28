2770dfba2bdb1e1acdc168528aeac99c8496b526
petercrackthecode <nguyenvandung02051999@gmail.com>
2020-03-27 19:19:26 -0700

    Added mapbox

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 import logo from "./logo.svg";
 import "./App.css";
 
 import Map from "./Map";
 
+const MAPBOX_TOKEN= 'pk.eyJ1IjoicGV0ZXJjcmFja3RoZWNvZGUiLCJhIjoiY2s4YXo0N3MwMDNjbjNvcXVyNWhqd2RkZCJ9.eexbGa1jw1QnZTSRpK2yPg';
+
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 37.6777879,
+    longitude: -121.75372929999999,
+    zoom: 10,
+    width: '100vw',
+    height: '100vh',
+    display: 'absolute'
+  });
   return (
     <div>
-      <Map />
+      <ReactMapGL {...viewport} mapboxApiAccessToken={MAPBOX_TOKEN} onViewportChange={viewport => {setViewport(viewport);}} />
     </div>
   );
 }
